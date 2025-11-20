e962969ce68f17c647b865c1df661a946d315081
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-20 18:16:32 +0800

    chore: formatting

--- home/gui/hyprland/default.nix
+++ home/gui/hyprland/default.nix
@@ -4,20 +4,23 @@
       enable = true;
       settings = {
         "$mod" = "SUPER";
-	bind = [
-	  "$mod, F, exec, firefox"
-	  "$mod, Return, exec, ghostty"
-	  "$mod, Q, killactive"
-	] ++ (
-	  builtins.concatLists  (builtins.genList  (i:
-	    let ws = i + 1;
-	    in [
-	      "$mod, code:1${toString i}, workspace, ${toString  ws}"
-	      "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-	    ]
-	  )
-	  9)
-	);
+        bind = [
+          "$mod, F, exec, firefox"
+          "$mod, Return, exec, ghostty"
+          "$mod, Q, killactive"
+        ]
+        ++ (builtins.concatLists (
+          builtins.genList (
+            i:
+            let
+              ws = i + 1;
+            in
+            [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          ) 9
+        ));
       };
       package = null;
       portalPackage = null;
