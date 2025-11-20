d03420db20a15c8b29e4c2c7d7825b827a684784
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-21 04:30:41 +0800

    feat(hyprland/bind): window management binds

--- home/gui/hyprland/bind.nix
+++ home/gui/hyprland/bind.nix
@@ -10,8 +10,24 @@ in
           "${mod}, F, exec, firefox"
           "${mod}, Return, exec, ghostty"
           "${mod}, Q, killactive"
-	  "${mod}, Y, fullscreen"
-	  "${mod}, D, exec, discord"
+          "${mod}, Y, fullscreen"
+          "${mod}, D, exec, discord"
+
+          # Window actions
+          # Click and drag
+          "${mod}, mouse:272, movewindow"
+
+          # Direction keys
+          "${mod} SHIFT, left, movewindow, l"
+          "${mod} SHIFT, right, movewindow, r"
+          "${mod} SHIFT, up, movewindow, u"
+          "${mod} SHIFT, down, movewindow, d"
+
+          # Focus
+          "${mod}, left, movefocus, l"
+          "${mod}, right, movefocus, r"
+          "${mod}, up, movefocus, u"
+          "${mod}, down, movefocus, d"
         ]
         ++ (builtins.concatLists (
           builtins.genList (
