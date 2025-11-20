7969fc35f9ddb89f6378936609bbaed140273119
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-20 20:45:21 +0800

    feat: colorscheme config and helper

--- flake.nix
+++ flake.nix
@@ -23,19 +23,43 @@
     }:
     {
       nixosConfigurations.athenaeum = nixpkgs.lib.nixosSystem {
-      	specialArgs = { inherit inputs;} ;
+        specialArgs = { inherit inputs; };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users = {
-                serenity = ./home;
+          (
+            let
+              colorscheme = {
+                crust = "#11111b";
+                mantle = "#181825";
+                base = "#1e1e2e";
+
+                text = "#cdd6f4";
+                mauve = "#cba6f7";
+                hexToARGB =
+                  hexString:
+                  let
+                    trimmedHex = nixpkgs.lib.strings.removePrefix "#" hexString;
+                  in
+                  if nixpkgs.lib.strings.stringLength trimmedHex == 6 then
+                    "0xFF${trimmedHex}"
+                  else
+                    throw "Input must be a 6-digit hex code (e.g., '#FFFFFF' or 'FFFFFF')";
               };
-            };
-          }
+            in
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users = {
+                  serenity = ./home;
+                };
+                extraSpecialArgs = {
+                  inherit colorscheme;
+                };
+              };
+            }
+          )
         ];
       };
     };
