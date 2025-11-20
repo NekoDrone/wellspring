a6f4aa426d850f917f8f714874c5cc9259c57ecb
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-11-21 02:52:31 +0800

    refactor(discord/moonlight): import config

--- home/apps/discord/default.nix
+++ home/apps/discord/default.nix
@@ -4,7 +4,26 @@
     inputs.moonlight.homeModules.default
   ];
 
-  programs.moonlight-mod = {
+  programs.moonlight = {
     enable = true;
+    configs.stable = {
+      extensions = {
+        moonbase = true;
+        disableSentry = true;
+        noTrack = true;
+        noHideToken = true;
+        moonlight-css = {
+          enabled = true;
+          config = {
+            paths = [
+              "/home/serenity/.config/moonlight-mod/themes"
+            ];
+          };
+        };
+      };
+      repositories = [
+        "https://moonlight-mod.github.io/extensions-dist/repo.json"
+      ];
+    };
   };
 }
