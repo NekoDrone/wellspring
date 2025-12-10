2f68fc2a7b6ca1a1f9dc47450fca32969e3464b2
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-12-10 19:26:58 +0800

    refactor(hyprland/animations): redo beziers

--- home/gui/hyprland/animations.nix
+++ home/gui/hyprland/animations.nix
@@ -4,13 +4,14 @@
       settings = {
         animations = {
           enabled = true;
-          bezier = "overshot, 0.13, 0.99, 0.29, 1.1";
+          bezier = "overshot, 0.05, 0.9, 0.1, 1.1";
           animation = [
-            "windowsIn, 1, 4, overshot, slide"
-            "windowsOut, 1, 5, default, popin 80%"
-            "border, 1, 5, default"
-            "workspacesIn, 1, 6, overshot, slide"
-            "workspacesOut, 1, 6, overshot, slidefade 80%"
+            "windowsIn, 1, 1.5, overshot, slide"
+            "windowsOut, 1, 1.5, default, popin 80%"
+	    "windowsMove, 1, 1.5, default, slide"
+            "border, 1, 1.5, default"
+            "workspacesIn, 1, 3, overshot, slide"
+            "workspacesOut, 1, 3, overshot, slidefade 80%"
           ];
         };
       };
