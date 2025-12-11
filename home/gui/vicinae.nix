c6c4e7bb382ac27dbf7c6443335f64e635f9aa81
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-12-12 01:33:17 +0800

    feat(vicinae): provide config

--- home/gui/vicinae.nix
+++ home/gui/vicinae.nix
@@ -1,5 +1,28 @@
 {
   programs.vicinae = {
     enable = true;
+    settings = {
+      closeOnFocusLoss = false;
+      faviconService = "twenty";
+      font = {
+        normal = "Sans Serif";
+        size = 10.5;
+      };
+      keybinding = "default";
+      keybinds = {
+      };
+      popToRootOnClose = true;
+      rootSearch = {
+        searchFiles = false;
+      };
+      theme = {
+        name = "catppuccin-mocha";
+      };
+      window = {
+        csd = true;
+        opacity = 0.95;
+        rounding = 10;
+      };
+    };
   };
 }
