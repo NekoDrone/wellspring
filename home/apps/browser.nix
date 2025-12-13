9d6a0c7cccb6a4678c66c3f7bf11be0540d5e638
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-12-13 10:16:26 +0800

    feat(browser): add small preferences config

--- home/apps/browser.nix
+++ home/apps/browser.nix
@@ -1,6 +1,28 @@
 { inputs, ... }:
+let
+  mkLockedAttrs = builtins.mapAttrs (
+    _: value: {
+      Value = value;
+      Status = "locked";
+    }
+  );
+  preferencesConfig = mkLockedAttrs {
+    "zen.view.use-single-toolbar" = true;
+    "zen.welcome-screen.seen" = true;
+  };
+in
 {
   imports = [ inputs.zen-browser.homeModules.beta ];
 
-  programs.zen-browser.enable = true;
+  programs.zen-browser = {
+    enable = true;
+
+    profiles.serenity = {
+      settings = preferencesConfig;
+    };
+
+    policies = {
+      Preferences = preferencesConfig;
+    };
+  };
 }
