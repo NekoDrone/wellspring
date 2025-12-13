a23e6ea1c1fdb67db7988e4ba8928d105366d016
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-12-13 10:16:08 +0800

    feat(hyprland/autostart): system wide darkmode gtk

--- home/gui/hyprland/autostart.nix
+++ home/gui/hyprland/autostart.nix
@@ -5,6 +5,10 @@
         exec-once = [
 	  "vicinae server"
         ];
+	exec = [
+	  "gsettings set org.gnome.desktop.interface gtk-theme \"YOUR_DARK_GTK3_THEME\""
+	  "gsettings set org.gnome.desktop.interface color-scheme \"prefer-dark\""
+	];
       };
     };
   };
