75de1cd03594f1910fd535b8e7279b11861bc65f
serenity <98162476+NekoDrone@users.noreply.github.com>
2025-12-25 20:58:34 +0800

    refactor(steam): additional packages

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -5,5 +5,20 @@
     extraCompatPackages = [
       pkgs.proton-ge-bin
     ];
+    package =
+      with pkgs;
+      steam.override {
+        extraPkgs = pkgs: [
+          jq
+          cabextract
+          wget
+          git
+          pkgsi686Linux.libpulseaudio
+          pkgsi686Linux.freetype
+          pkgsi686Linux.xorg.libXcursor
+          pkgsi686Linux.xorg.libXcomposite
+          pkgsi686Linux.xorg.libXi
+        ];
+      };
   };
 }
