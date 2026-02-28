9afaa7de28540ca0721a1819bb61a58df8c0c3c5
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-02-28 23:02:26 +0800

    fix(gpg): kill gpgconf after loading sops

--- home/services/gpg.nix
+++ home/services/gpg.nix
@@ -19,6 +19,7 @@
   home.activation.importGpgKey = config.lib.dag.entryAfter [ "writeBoundary" ] ''
     if [ -f ${nixosConfig.sops.secrets.key-gpg.path} ]; then
       ${pkgs.gnupg}/bin/gpg --import ${nixosConfig.sops.secrets.key-gpg.path} 2>/dev/null || true
+      ${pkgs.gnupg}/bin/gpgconf --kill all 2>/dev/null || true
     fi
   '';
 }
