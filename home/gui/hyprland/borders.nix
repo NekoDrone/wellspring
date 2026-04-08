c70e4456a198f048b719c581d5c0f463386ed374
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-04-08 21:12:28 +0800

    fix(hyprland): remove option

--- home/gui/hyprland/borders.nix
+++ home/gui/hyprland/borders.nix
@@ -2,7 +2,6 @@
 {
   wayland.windowManager.hyprland.settings.general = {
     border_size = 2;
-    no_border_on_floating = true;
     "col.active_border" = colorscheme.hexToARGB colorscheme.mauve;
     "col.inactive_border" = colorscheme.hexToARGB colorscheme.base;
   };
