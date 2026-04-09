149149d46a207aa683bf1b95430ae5a54cacc7a1
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-04-09 23:10:44 +0800

    fix(networking): remove options

--- system/networking.nix
+++ system/networking.nix
@@ -4,23 +4,9 @@
     networkmanager = {
       enable = true;
       dns = "systemd-resolved";
-      wifi = {
-        powersave = false;
-        macAddress = "random";
-      };
-    };
-    firewall = {
-      enable = true;
-      allowPing = false;
     };
   };
   services.resolved = {
     enable = true;
-    settings = {
-      Resolve = {
-        DNSSEC = "allow-downgrade";
-        DNSOverTLS = "opportunistic";
-      };
-    };
   };
 }
