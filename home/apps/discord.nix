c621ebffbcded894037cffbda89900344b34ad50
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-04-11 22:57:06 +0800

    refactor(discord): swap moonlight to vesktop

--- home/apps/discord.nix
+++ home/apps/discord.nix
@@ -1,29 +1,56 @@
-{ inputs, pkgs, ... }:
 {
-  imports = [
-    inputs.moonlight.homeModules.default
-  ];
-
-  programs.moonlight = {
-    enable = true;
-    configs.stable = {
-      extensions = {
-        moonbase = true;
-        disableSentry = true;
-        noTrack = true;
-        noHideToken = true;
-        moonlight-css = {
-          enabled = true;
-          config = {
-            paths = [
-              "/home/serenity/.config/moonlight-mod/themes"
-            ];
+  ...
+}:
+{
+  programs.vesktop =
+    let
+      cfg = {
+        light = {
+          flavour = "mocha";
+        };
+        dark = {
+          flavour = "mocha";
+        };
+        accent = "mauve";
+      };
+      themeName = "catppuccin-auto.theme";
+    in
+    {
+      enable = true;
+      vencord = {
+        themes.${themeName} = ''
+          /**
+           * @name Catppuccin ${cfg.light.flavour}/${cfg.dark.flavour} (${cfg.accent})
+           * @author Catppuccin
+           * @description Soothing pastel theme for Discord
+           * @website https://github.com/catppuccin/discord
+          **/
+          @import url("https://catppuccin.github.io/discord/dist/catppuccin-${cfg.dark.flavour}-${cfg.accent}.theme.css")
+          (prefers-color-scheme: dark);
+          @import url("https://catppuccin.github.io/discord/dist/catppuccin-${cfg.light.flavour}-${cfg.accent}.theme.css")
+          (prefers-color-scheme: light);
+        '';
+        settings = {
+          autoUpdate = true;
+          autoUpdateNotification = false;
+          notifyAboutUpdates = false;
+          customTitleBar = true;
+          enabledThemes = [ "${themeName}.css" ];
+          plugins = {
+            BetterGifPicker = {
+              enabled = true;
+            };
+            FavoriteGifSearch = {
+              enabled = true;
+            };
+            CallTimer = {
+              enabled = true;
+            };
+            WhoReacted = {
+              enabled = true;
+            };
           };
         };
       };
-      repositories = [
-        "https://moonlight-mod.github.io/extensions-dist/repo.json"
-      ];
     };
-  };
 }
