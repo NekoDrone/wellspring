b28a5b716e53f4a43b6c31608c8e191891701376
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-06-04 17:40:29 +0800

    refactor(inputs)!: remove hyprland flake

--- flake.nix
+++ flake.nix
@@ -9,10 +9,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # hyprland = {
-    #   url = "github:hyprwm/Hyprland";
-    # };
-
     niri = {
       url = "github:sodiboo/niri-flake";
       inputs.nixpkgs.follows = "nixpkgs";
